73d98592509eef0e4f0dc6c9a989d46a51f24c8d
AnotherRequiem <onerequiem@yahoo.com>
2024-02-06 11:55:36 +0200

    post, put, delete endpoints added

--- Requiem.PetProject/Controllers/StockController.cs
+++ Requiem.PetProject/Controllers/StockController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Requiem.PetProject.Data;
 using Requiem.PetProject.DTOs.Stock;
 using Requiem.PetProject.Mappers;
@@ -17,18 +18,18 @@ public class StockController : ControllerBase
     }
 
     [HttpGet]
-    public IActionResult GetAll()
+    public async Task<IActionResult> GetAll()
     {
-        var stocks = _context.Stocks.ToList()
-            .Select(s => s.ToStockDto());
+        var stocks = await _context.Stocks.ToListAsync();
+        var stockDto = stocks.Select(s => s.ToStockDto());
 
         return Ok(stocks);
     }
 
     [HttpGet("{id}")]
-    public IActionResult GetById([FromRoute] Guid id)
+    public async Task<IActionResult> GetById([FromRoute] Guid id)
     {
-        var stock = _context.Stocks.Find(id);
+        var stock = await _context.Stocks.FindAsync(id);
 
         if (stock == null)
         {
@@ -39,9 +40,52 @@ public class StockController : ControllerBase
     }
 
     [HttpPost]
-    public IActionResult Create([FromBody] CreateStockRequestDto stockDto)
+    public async Task<IActionResult> Create([FromBody] CreateStockRequestDto createDto)
     {
-        return Ok();
+        var stockModel = createDto.ToStockFromCreateDto();
+        await _context.Stocks.AddAsync(stockModel);
+        await _context.SaveChangesAsync();
+        
+        return CreatedAtAction(nameof(GetById), new {id = stockModel.Id}, stockModel.ToStockDto());
     }
-    
+
+    [HttpPut]
+    [Route("{id}")]
+    public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] UpdateStockRequestDto updateDto)
+    {
+        var stockModel = await _context.Stocks.FirstOrDefaultAsync(s => s.Id == id);
+
+        if (stockModel == null)
+        {
+            return NotFound();
+        }
+
+        stockModel.Symbol = updateDto.Symbol;
+        stockModel.CompanyName = updateDto.CompanyName;
+        stockModel.Purchase = updateDto.Purchase;
+        stockModel.LastDiv = updateDto.LastDiv;
+        stockModel.Industry = updateDto.Industry;
+        stockModel.MarketCap = updateDto.MarketCap;
+
+        await _context.SaveChangesAsync();
+
+        return Ok(stockModel.ToStockDto());
+    }
+
+    [HttpDelete]
+    [Route("{id}")]
+    public async Task<IActionResult> Delete([FromRoute] Guid id)
+    {
+        var stockModel = await _context.Stocks.FirstOrDefaultAsync(s => s.Id == id);
+
+        if (stockModel == null)
+        {
+            return NotFound();
+        }
+
+        _context.Stocks.Remove(stockModel);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
 }
